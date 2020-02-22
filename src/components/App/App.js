a5e3c9e058dc1db36fa7912ed7462cef9a4e72b9
Alexander Balandin <my@maggit.ru>
2020-02-22 11:29:36 +0300

    Change structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import logo from './logo.svg'
+import logo from 'assets/images/logo.svg'
 import './App.css'
 
-function App () {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +22,3 @@ function App () {
     </div>
   )
 }
-
-export default App
