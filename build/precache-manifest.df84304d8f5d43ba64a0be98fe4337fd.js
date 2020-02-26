a85a4ff5802803ee079bb7f3f3c2340830cfbb7c
Alexander Balandin <my@maggit.ru>
2020-02-26 14:32:34 +0300

    build

---
+++ build/precache-manifest.df84304d8f5d43ba64a0be98fe4337fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1183d42af6bd250f2ab27f35028f2087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "217f3c8cb935707b4797",
+    "url": "/static/css/main.f0d918a6.chunk.css"
+  },
+  {
+    "revision": "52b31f615731d560f0a1",
+    "url": "/static/js/2.86181f88.chunk.js"
+  },
+  {
+    "revision": "1bc405a2f9ca5b8136d11cf29f9b2788",
+    "url": "/static/js/2.86181f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217f3c8cb935707b4797",
+    "url": "/static/js/main.24ad6c28.chunk.js"
+  },
+  {
+    "revision": "f8d9ac9b5a86ceac0bfe",
+    "url": "/static/js/runtime-main.6c4ba166.js"
+  }
+]);
