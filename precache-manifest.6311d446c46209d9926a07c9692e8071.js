6a8b1edfba6dedcdb2fea8fb52bd5b40e9a0ea92
Alexander Balandin <my@maggit.ru>
2020-06-14 09:53:03 +0300

    Updates

---
+++ precache-manifest.6311d446c46209d9926a07c9692e8071.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ec1746bc600ff4e4a4aaded07ccc58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1798af6e03256619ae40",
+    "url": "/static/css/main.4c1e398a.chunk.css"
+  },
+  {
+    "revision": "221a45973f6d199bdaa3",
+    "url": "/static/js/2.94a0ba51.chunk.js"
+  },
+  {
+    "revision": "f2d17e9080d0a039a01053338b681c14",
+    "url": "/static/js/2.94a0ba51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1798af6e03256619ae40",
+    "url": "/static/js/main.3c51f41f.chunk.js"
+  },
+  {
+    "revision": "47d240bc0e30170d28e8",
+    "url": "/static/js/runtime-main.427a821e.js"
+  },
+  {
+    "revision": "5bced02a3c36de14a122eb366d62f3b4",
+    "url": "/static/media/pic1.5bced02a.png"
+  },
+  {
+    "revision": "57a2f3dd65bee858e8234240c7ada2e7",
+    "url": "/static/media/pic2.57a2f3dd.jpg"
+  },
+  {
+    "revision": "5c3a90ab6dc53c9b8eeea42037451586",
+    "url": "/static/media/pic3.5c3a90ab.jpg"
+  },
+  {
+    "revision": "43b87bdba317c7e7cd4dfed526fa17f0",
+    "url": "/static/media/pic4.43b87bdb.jpg"
+  }
+]);
